aa5b71dbf4fedc11b16fdcd306efa4c198d37e36
Yashika-97 <ygarg5618@gmail.com>
2025-07-28 15:14:00 +0530

    Update Findid.js

--- client/src/components/Findid.js
+++ client/src/components/Findid.js
@@ -11,7 +11,7 @@ export function Findid(){
         //e.preventDefault();
         try
         {
-const response = await axios.get(`http://localhost:3001/api/employees/${id}`);
+const response = await axios.get(`https://new-project-fh3u.onrender.com/api/employees/${id}`);
 setEmployee(response.data);
         }
         catch(err)
